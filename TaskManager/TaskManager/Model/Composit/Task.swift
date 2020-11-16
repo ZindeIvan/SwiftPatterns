f054b8c553ea6bba4a2772d506dd57cc210ce90b
ZindeIvan <zinde.ivan@yandex.ru>
2020-11-16 23:55:31 +0600

    init pattern composite

---
+++ TaskManager/TaskManager/Model/Composit/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TaskManager
+//
+//  Created by Зинде Иван on 11/16/20.
+//  Copyright © 2020 Зинде Иван. All rights reserved.
+//
+
+import Foundation
+//Протокол Задачи
+protocol Task {
+    var name: String {get set}
+}
